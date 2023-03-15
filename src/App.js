b4388acc090394b79b74c3542a9bcbf4439c00fd
nirgluzman <nir.gluzman@gmail.com>
2023-03-15 23:38:22 +0100

    add some logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+
+  const doubleNumber = slowFunction(number);
+
+  const themeStyles = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
 
   return (
     <>
-      <div>count: {count}</div>
-      <button onClick={() => setCount(count + 1)}>increment</button>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
     </>
   );
+}
+
+function slowFunction(num) {
+  console.log("Calling Slow Function");
+  for (let i = 0; i < 1000000000; i++) {}
+  return num * 2;
 }
 
 export default App;
